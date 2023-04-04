90c44cfe0b0b8b71c303c85c08a681a5aa380e3f
Connor Zanin <cnnrznn@gmail.com>
2023-04-04 00:34:26 -0600

    Accept uri and filename args

--- main.go
+++ main.go
@@ -2,19 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
 type Main struct {
 	isServer, isClient bool
+	filename           string
+	toURI              string
 }
 
 func main() {
 	main := &Main{}
 
 	if err := parseArgs(main); err != nil {
-		fmt.Print(err)
-		os.Exit(1)
+		log.Fatalf("Bad args: %v", err)
+	}
+
+	if main.isServer {
+		// start server
+	}
+
+	if main.isClient {
+		// start client
 	}
 
 	os.Exit(0)
@@ -23,7 +33,7 @@ func main() {
 func parseArgs(main *Main) error {
 	args := os.Args
 
-	for _, arg := range args {
+	for i, arg := range args {
 		switch arg {
 		case "-s":
 			main.isServer = true
@@ -32,11 +42,22 @@ func parseArgs(main *Main) error {
 		case "-b":
 			main.isClient = true
 			main.isServer = true
+		default:
+			main.toURI = args[i]
+			main.filename = args[i+1]
 		}
 	}
 
 	if !main.isClient && !main.isServer {
-		return fmt.Errorf("Bad args, specify server, or client, or both")
+		return fmt.Errorf("specify server, client, or both")
+	}
+
+	if len(main.toURI) == 0 {
+		return fmt.Errorf("specify a destination URI")
+	}
+
+	if len(main.filename) == 0 {
+		return fmt.Errorf("specify a filename to send/receive")
 	}
 
 	return nil
