960baad28b3a5a32951b7e7fab3ddc7fcd8a832b
Connor Zanin <cnnrznn@gmail.com>
2023-04-05 00:28:57 -0600

    Create the metadata from the file in the client

--- client/client.go
+++ client/client.go
@@ -1,5 +1,14 @@
 package client
 
+import (
+	"bufio"
+	"crypto/sha256"
+	"fmt"
+	"os"
+
+	"github.com/cnnrznn/goftp/model"
+)
+
 type Client struct {
 	filename string
 	toURI    string
@@ -15,6 +24,68 @@ func New(
 	}
 }
 
-func (s *Client) Run(stopChan chan error) {
-	// do stuff
+func (c *Client) Run(stopChan chan error) {
+	meta, err := c.createMetadata()
+	if err != nil {
+		stopChan <- err
+		return
+	}
+
+	if err := c.sendFile(meta); err != nil {
+		stopChan <- err
+		return
+	}
+}
+
+func (c *Client) createMetadata() (*model.Meta, error) {
+	meta := &model.Meta{}
+
+	file, err := os.Open(c.filename)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't open file %v", c.filename)
+	}
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	meta.Size = int(stat.Size())
+	meta.Name = stat.Name()
+	meta.Checksum, err = calculateChecksum(file, int(stat.Size()))
+	if err != nil {
+		return nil, err
+	}
+
+	return meta, nil
+}
+
+func (c *Client) sendFile(meta *model.Meta) error {
+	return nil
+}
+
+func calculateChecksum(file *os.File, size int) (string, error) {
+	hasher := sha256.New()
+	buf := make([]byte, 8192)
+	reader := bufio.NewReader(file)
+	nread := 0
+
+	for nread < size {
+		n, err := reader.Read(buf)
+		if err != nil {
+			return "", err
+		}
+
+		nread += n
+		nwritten, err := hasher.Write(buf[:n])
+		if err != nil {
+			return "", err
+		}
+		if n != nwritten {
+			return "", fmt.Errorf("failed to write all bytes to hasher")
+		}
+	}
+
+	return string(hasher.Sum(nil)), nil
 }
